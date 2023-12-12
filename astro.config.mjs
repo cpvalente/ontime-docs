64cc342a1ada90e37d7f4d5dfe2221c4fad91983
Carlos Valente <carlosvalente@pm.me>
2023-12-12 21:15:38 +0100

    docs: initial documentation migration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,53 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlightLinksValidator(),
+    starlight({
+      title: "the-docs",
+      customCss: ["./src/styles/custom.css"],
+      social: {
+        github: "https://github.com/cpvalente/ontime",
+      },
+      sidebar: [
+        {
+          label: "Ontime",
+          autogenerate: { directory: "ontime" },
+        },
+        {
+          label: "Interface",
+          autogenerate: { directory: "interface" },
+        },
+        {
+          label: "Features",
+          autogenerate: { directory: "features" },
+        },
+        {
+          label: "Views",
+          autogenerate: { directory: "views",  },
+        },
+        {
+          label: "API - Feedback and control",
+          autogenerate: { directory: "api" },
+        },
+        {
+          label: "Additional Notes",
+          autogenerate: { directory: "additional-notes" },
+        },
+        {
+          label: "External links",
+          items: [
+            { label: "Ontime website", link: "https://www.getontime.no" },
+            {
+              label: "Github Repo",
+              link: "https://www.github.com/cpvalente/ontime",
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
