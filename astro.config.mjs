47127ffe9e3698a3e13787c2efa5feab72592cfb
Carlos Valente <34649812+cpvalente@users.noreply.github.com>
2023-12-14 20:54:03 +0100

    chore: add vercel deployment (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightLinksValidator from 'starlight-links-validator'
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercelStatic(),
   integrations: [
     starlightLinksValidator(),
     starlight({
