9b005d6e028a84fadf65e133355be13291bc5b7c
Carlos Valente <carlosvalente@pm.me>
2025-04-19 12:23:56 +0200

    chore: upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,112 +5,120 @@ import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
-	output: "static",
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
-	site: "https://docs.getontime.no",
-	integrations: [
-		starlight({
-			plugins: [starlightLinksValidator()],
-			title: "Ontime documentation",
-			favicon: "./favicon.ico",
-			logo: {
-				src: "./src/assets/images/icon.webp",
-				replacesTitle: true,
-			},
-			customCss: ["./src/styles/custom.css"],
-			social: {
-				github: "https://github.com/cpvalente/ontime",
-				discord: "https://discord.com/invite/eje3CSUEXm",
-			},
-			editLink: {
-				baseUrl: "https://github.com/cpvalente/ontime-docs/edit/main",
-			},
-			sidebar: [
-				{
-					label: "Ontime",
-					autogenerate: { directory: "ontime" },
-				},
-				{
-					label: "Ontime Cloud",
-					autogenerate: { directory: "ontime-cloud" },
-				},
-				{
-					label: "Interface",
-					items: [
-						{
-							label: "Interface overview",
-							link: "/interface/",
-						},
-						{
-							label: "Production views",
-							autogenerate: { directory: "interface/production" },
-						},
-						{
-							label: "Backstage views",
-							autogenerate: { directory: "interface/backstage" },
-						},
-						{
-							label: "Public views",
-							autogenerate: { directory: "interface/public" },
-						},
-					],
-				},
-				{
-					label: "Features",
-					autogenerate: { directory: "features" },
-				},
-				{
-					label: "Feedback and control",
-					items: [
-						{
-							label: "API overview",
-							link: "/api/",
-						},
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  site: "https://docs.getontime.no",
+  integrations: [
+    starlight({
+      plugins: [starlightLinksValidator()],
+      title: "Ontime documentation",
+      favicon: "./favicon.ico",
+      logo: {
+        src: "./src/assets/images/icon.webp",
+        replacesTitle: true,
+      },
+      customCss: ["./src/styles/custom.css"],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/cpvalente/ontime",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.com/invite/eje3CSUEXm",
+        },
+      ],
+      editLink: {
+        baseUrl: "https://github.com/cpvalente/ontime-docs/edit/main",
+      },
+      sidebar: [
+        {
+          label: "Ontime",
+          autogenerate: { directory: "ontime" },
+        },
+        {
+          label: "Ontime Cloud",
+          autogenerate: { directory: "ontime-cloud" },
+        },
+        {
+          label: "Interface",
+          items: [
+            {
+              label: "Interface overview",
+              link: "/interface/",
+            },
+            {
+              label: "Production views",
+              autogenerate: { directory: "interface/production" },
+            },
+            {
+              label: "Backstage views",
+              autogenerate: { directory: "interface/backstage" },
+            },
+            {
+              label: "Public views",
+              autogenerate: { directory: "interface/public" },
+            },
+          ],
+        },
+        {
+          label: "Features",
+          autogenerate: { directory: "features" },
+        },
+        {
+          label: "Feedback and control",
+          items: [
+            {
+              label: "API overview",
+              link: "/api/",
+            },
 
-						{
-							label: "Data",
-							autogenerate: { directory: "api/data" },
-						},
-						{
-							label: "Automation",
-							autogenerate: { directory: "api/automation" },
-						},
-						{
-							label: "Protocol API",
-							autogenerate: { directory: "api/protocols" },
-						},
-					],
-				},
-				{
-					label: "Quick tips",
-					autogenerate: { directory: "quick-tips" },
-				},
-				{
-					label: "Additional notes",
-					autogenerate: { directory: "additional-notes" },
-				},
-				{
-					label: "External links",
-					items: [
-						{
-							label: "Ontime website",
-							link: "https://www.getontime.no",
-							badge: "Link",
-							attrs: { target: "_blank" },
-						},
-						{
-							label: "Github Repo",
-							link: "https://www.github.com/cpvalente/ontime",
-							badge: "Link",
-							attrs: { target: "_blank" },
-						},
-					],
-				},
-			],
-		}),
-	],
+            {
+              label: "Data",
+              autogenerate: { directory: "api/data" },
+            },
+            {
+              label: "Automation",
+              autogenerate: { directory: "api/automation" },
+            },
+            {
+              label: "Protocol API",
+              autogenerate: { directory: "api/protocols" },
+            },
+          ],
+        },
+        {
+          label: "Quick tips",
+          autogenerate: { directory: "quick-tips" },
+        },
+        {
+          label: "Additional notes",
+          autogenerate: { directory: "additional-notes" },
+        },
+        {
+          label: "External links",
+          items: [
+            {
+              label: "Ontime website",
+              link: "https://www.getontime.no",
+              badge: "Link",
+              attrs: { target: "_blank" },
+            },
+            {
+              label: "Github Repo",
+              link: "https://www.github.com/cpvalente/ontime",
+              badge: "Link",
+              attrs: { target: "_blank" },
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
